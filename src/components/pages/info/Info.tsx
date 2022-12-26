3f8f496d2ae7987868b17a722af8a459ffc69ea0
Angelo Dias <angelo@foobar.agency>
2022-12-26 12:51:07 +0100

    Complete textual information in Info page

--- src/components/pages/info/Info.tsx
+++ src/components/pages/info/Info.tsx
@@ -1,3 +1,4 @@
+import { H2, H3 } from "components/typography/typography"
 import { parseRichText } from "i18n/parse-rich-text"
 import { useTranslations } from "next-intl"
 import React from "react"
@@ -37,32 +38,40 @@ const archetypes = [
 export const InfoPage = () => {
   const t = useTranslations("pages.info")
   const archetypeTranslations = useTranslations("archetypes")
+
   return (
-    <>
-      <div>
-        <h2>{t("info.title")}</h2>
-        <>{t.rich("info.rich-text-content", parseRichText)}</>
+    <div className="gap-8 p-6 custom-outer-box">
+      {/* Info box */}
+      <div className="flex flex-col gap-3">
+        <H2 className="">{t("info.title")}</H2>
+        <div className="custom-inner-box">
+          {t.rich("info.rich-text-content", parseRichText)}
+        </div>
       </div>
-      <div>
-        <h2>{t("archetypes.title")}</h2>
-        <>{t.rich("archetypes.rich-text-content", parseRichText)}</>
+
+      {/* Archetypes box */}
+      <div className="flex flex-col gap-3">
+        <H2>{t("archetypes.title")}</H2>
+        <div className="custom-inner-box">
+          {t.rich("archetypes.rich-text-content", parseRichText)}
+        </div>
         {archetypes.map((archetype) => {
           const name = `${archetype}.name`
           const description = `${archetype}.description`
           return (
-            <div key={archetype}>
+            <div key={archetype} className="custom-inner-box">
               {/* Typescript is ignored below because of the lack of types
                 to typecast name and description. An issue was opened.
                 https://github.com/amannn/next-intl/issues/159
               */}
               {/* @ts-ignore */}
-              <h3>{archetypeTranslations(name)}</h3>
+              <H3>{archetypeTranslations(name)}</H3>
               {/* @ts-ignore */}
               <p>{archetypeTranslations(description)}</p>
             </div>
           )
         })}
       </div>
-    </>
+    </div>
   )
 }
