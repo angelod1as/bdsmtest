f712d85fcddd0d28a7e97975237884c31a94840a
Angelo Dias <angelo@foobar.agency>
2023-04-01 16:21:37 +0200

    Add test emails

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,58 +1,79 @@
 import { Box } from "components/Box"
 import { Button } from "components/Button"
+import { Error } from "components/Error"
 import { Input } from "components/Input"
-import { Form, Formik, FormikHelpers } from "formik"
+import { Form, Formik } from "formik"
 import { callApi } from "helpers/callApi"
 import { ROUTES } from "helpers/routes"
 import { useTranslations } from "next-intl"
-import React from "react"
+import React, { useState } from "react"
 import * as yup from "yup"
 
-type FormProps = {
+export type LoginFormProps = {
   email?: string
+  password?: string
 }
 
-const validationSchema = yup.object().shape({
-  email: yup.string().email("Not a proper email").required("Required"),
-})
-
 export const LoginPage = () => {
+  const [submitError, setSubmitError] = useState("")
   const t = useTranslations("pages.login")
 
+  const handleFocus = () => setSubmitError("")
+
+  const validationSchema = yup.object().shape({
+    email: yup.string().email(t("login.email.error")).required("Required"),
+    password: yup.string().required(),
+  })
+
   // Must be async
-  const handleSubmit = async (
-    values: FormProps,
-    formikHelpers: FormikHelpers<FormProps>,
-  ) => {
+  const handleSubmit = async (values: LoginFormProps) => {
     try {
-      await callApi.post({
+      const response = await callApi.post({
         url: ROUTES.api.login,
         body: values,
       })
+      if (response.ok === true) {
+        return
+      }
+
+      // TODO: Proper error message
+      setSubmitError(t("login.error"))
     } catch (error) {
       console.error(error)
-      formikHelpers.setErrors({ email: t("login.error") })
+      setSubmitError(t("login.error"))
     }
   }
 
   return (
     <Box type="outer">
-      <Formik<FormProps>
-        initialValues={{ email: "" }}
+      <Formik<LoginFormProps>
+        initialValues={{ email: "", password: "" }}
         onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
         {({ errors, values, isSubmitting, handleChange }) => (
           <Form className="flex flex-col gap-4 mt-4">
             <Input
-              label={t("login.enterEmail")}
+              label={t("login.email.label")}
               name="email"
               type="email"
               value={values.email}
               onChange={handleChange}
               error={errors.email}
+              onFocus={handleFocus}
             />
 
+            <Input
+              label={t("login.password.label")}
+              name="password"
+              type="password"
+              value={values.password}
+              onChange={handleChange}
+              error={errors.password}
+              onFocus={handleFocus}
+            />
+
+            {submitError && <Error>{submitError}</Error>}
             <Button variant="secondary" type="submit" disabled={isSubmitting}>
               {t("login.submit")}
             </Button>
--- src/pages/api/login.tsx
+++ src/pages/api/login.tsx
@@ -1,16 +1,38 @@
+import { LoginFormProps } from "components/pages/login/Login"
 import { NextApiRequest, NextApiResponse } from "next"
 import assert from "ow"
 
-export type responseItem = {}
+type RequestBody = LoginFormProps
+
+export type Response = {}
 
 export default async function loginHandler(
   req: NextApiRequest,
-  res: NextApiResponse<responseItem>,
+  res: NextApiResponse<Response>,
 ) {
-  const { email } = req.body
+  const body: RequestBody = JSON.parse(req.body)
+  const { email, password } = body
   assert(email, assert.string.not.empty)
+  assert(password, assert.string.not.empty)
 
-  // TODO: check email and respond
+  // TODO: Decifer the original code on how to do the API call and
+  // respond accordingly. This call skips the email check, a security bad practice.
 
-  res.send(200)
+  console.log("test")
+
+  if (email === "ok@test.com") {
+    return res.json({
+      ok: true,
+      status: "OK",
+    })
+  }
+
+  if (email === "not-ok@test.com") {
+    return res.status(500).json({
+      ok: false,
+      status: "not OK",
+    })
+  }
+
+  return res.status(500).send(new Error("Wrong test email"))
 }
