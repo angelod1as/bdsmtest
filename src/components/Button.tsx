deb76a1086a53bb5132a198b1d976cc5ac497e7a
Angelo Dias <angelo@foobar.agency>
2023-04-10 09:16:58 +0200

    Fix button classname

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,15 +6,19 @@ type ButtonProps = JSX.IntrinsicElements["button"] & {
   variant?: "primary" | "secondary"
 }
 
-export const Button: FCC<ButtonProps> = ({ variant = "primary", ...props }) => {
-  const className = {
+export const Button: FCC<ButtonProps> = ({
+  variant = "primary",
+  className,
+  ...props
+}) => {
+  const newClassName = {
     primary: "bg-blue text-white",
     secondary: "bg-darkgray border-lightgray",
   }[variant]
 
   return (
     <button
-      className={clx(props.className, className, "border rounded px-4")}
+      className={clx(className, newClassName, "border rounded px-4")}
       {...props}
     >
       {props.children}
