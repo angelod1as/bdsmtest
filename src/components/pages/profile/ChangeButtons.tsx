4231b7f15a29b4c4d748390b9438af092a8a724e
Angelo Dias <angelo@foobar.agency>
2023-04-10 09:33:23 +0200

    Add ChangeButtons functionality

--- src/components/pages/profile/ChangeButtons.tsx
+++ src/components/pages/profile/ChangeButtons.tsx
@@ -2,6 +2,7 @@ import { H4 } from "components/typography/typography"
 import { Button } from "components/Button"
 
 import { useTranslations } from "next-intl"
+import { navigate } from "helpers/navigate"
 
 export const ChangeButtons = () => {
   const t = useTranslations("pages.profile.content")
@@ -10,13 +11,25 @@ export const ChangeButtons = () => {
     <div>
       <H4 className="mb-2">{t("change-title")}</H4>
       <div className="flex gap-2">
-        <Button className="w-full" variant="secondary">
+        <Button
+          onClick={() => navigate("changeName")}
+          className="w-full"
+          variant="secondary"
+        >
           {t("change-name")}
         </Button>
-        <Button className="w-full" variant="secondary">
+        <Button
+          onClick={() => navigate("changePassword")}
+          className="w-full"
+          variant="secondary"
+        >
           {t("change-password")}
         </Button>
-        <Button className="w-full" variant="secondary">
+        <Button
+          onClick={() => navigate("changeEmail")}
+          className="w-full"
+          variant="secondary"
+        >
           {t("change-email")}
         </Button>
       </div>
